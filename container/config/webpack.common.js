38cf336f2c7179b83d90d9756372c5a7d3de783c
Nathan Doyle <natetronn@gmail.com>
2020-11-17 20:38:27 -0800

    Initial Commit

---
+++ container/config/webpack.common.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react', 
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime'
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: './public/index.html'
+        }),
+    ],
+};
